49c0a9154fe554e9bb9f7207e8b13eb6956f076d
Alex Berg <a.vandenberg-2@tudelft.nl>
2025-05-29 18:57:20 +0200

    starting conversion

--- runtime/source/data_loading.R
+++ runtime/source/data_loading.R
@@ -48,86 +48,88 @@ get_p_detail <- function(participant, detail) {
   return(detailValue)
 }
 
+calculate_stats <- function(x) {
+  x <- as.numeric(x)
+  stats <- list(
+    mean = mean(x, na.rm = TRUE),
+    sd = sd(x, na.rm = TRUE),
+    median = median(x, na.rm = TRUE),
+    min = min(x, na.rm = TRUE),
+    max = max(x, na.rm = TRUE),
+    iqr = IQR(x, na.rm = TRUE)
+  )
+  return(stats)
+}
+
 calculate_participant_details <- function(participants) {
   details <- data.frame(participant = participants)
-  
+
   # Retrieve details for each participant
   details$age <- sapply(participants, get_p_detail, detail = "age")
   details$gender <- sapply(participants, get_p_detail, detail = "gender")
-  details$startedWithNoise <- sapply(participants, started_with_noise)
+  details$weight <- sapply(participants, get_p_detail, detail = "weight")
   details$education <- sapply(participants, get_p_detail, detail = "education")
   details$vr_experience <- sapply(participants, get_p_detail, detail = "vr_experience")
   details$motion <- sapply(participants, get_p_detail, detail = "motion")
-  details$noticed <- sapply(participants, get_p_detail, detail = "noticed")
   details$move_speed <- sapply(participants, get_move_speed)
-  
+  details$condition <- sapply(participants, get_p_detail, detail = "condition")
+
   # Calculate statistics for age
-  age_mean <- mean(details$age)
-  age_sd <- sd(details$age)
-  age_median <- median(details$age)
-  age_min <- min(details$age)
-  age_max <- max(details$age)
-  age_iqr <- IQR(details$age)
-  
-  # Format the age results
-  age_mean_sd <- sprintf("%.2f, SD: %.2f", age_mean, age_sd)
-  age_median_min_max_iqr <- sprintf("%d [%.0f, %.0f], IQR=%.2f", age_median, age_min, age_max, age_iqr)
-  
+  age_stats <- calculate_stats(details$age)
+  age_mean_sd <- sprintf("%.2f, SD: %.2f", age_stats$mean, age_stats$sd)
+  age_median_min_max_iqr <- sprintf("%d [%.0f, %.0f], IQR=%.2f", age_stats$median, age_stats$min, age_stats$max, age_stats$iqr)
+
   # Calculate statistics for move speed
-  move_speed_mean <- mean(details$move_speed)
-  move_speed_sd <- sd(details$move_speed)
-  move_speed_median <- median(details$move_speed)
-  move_speed_min <- min(details$move_speed)
-  move_speed_max <- max(details$move_speed)
-  move_speed_iqr <- IQR(details$move_speed)
-  
-  # Format the move speed results
-  move_speed_mean_sd <- sprintf("%.2f, SD: %.2f", move_speed_mean, move_speed_sd)
-  move_speed_median_min_max_iqr <- sprintf("%.2f [%.2f, %.2f], IQR=%.2f", move_speed_median, move_speed_min, move_speed_max, move_speed_iqr)
-  
+  move_speed_stats <- calculate_stats(details$move_speed)
+  move_speed_mean_sd <- sprintf("%.2f, SD: %.2f", move_speed_stats$mean, move_speed_stats$sd)
+  move_speed_median_min_max_iqr <- sprintf("%.2f [%.2f, %.2f], IQR=%.2f", move_speed_stats$median, move_speed_stats$min, move_speed_stats$max, move_speed_stats$iqr)
+
+  # Calculate statistics for weight
+  weight_stats <- calculate_stats(details$weight)
+  weight_mean_sd <- sprintf("%.2f, SD: %.2f", weight_stats$mean, weight_stats$sd)
+  weight_median_min_max_iqr <- sprintf("%.2f [%.2f, %.2f], IQR=%.2f", weight_stats$median, weight_stats$min, weight_stats$max, weight_stats$iqr)
+
   # Count the occurrences of each unique value in other columns
   gender_counts <- table(details$gender)
-  started_counts <- table(details$startedWithNoise)
   education_counts <- table(details$education)
   vr_experience_counts <- table(details$vr_experience)
   motion_counts <- table(details$motion)
-  noticed_counts <- table(details$noticed)
-  
+  condition_counts <- table(details$condition)
+
   # Create a result data frame
   result <- data.frame(
     Detail = c(
       "Age (Mean, SD)", "Age (Median [Min, Max], IQR)",
       "Move Speed (Mean, SD)", "Move Speed (Median [Min, Max], IQR)",
+      "Weight (Mean, SD)", "Weight (Median [Min, Max], IQR)",
       rep("Gender", length(gender_counts)),
-      rep("Started With Noise", length(started_counts)),
       rep("Education", length(education_counts)),
       rep("VR Experience", length(vr_experience_counts)),
       rep("Motion", length(motion_counts)),
-      rep("Noticed", length(noticed_counts))
+      rep("Condition", length(condition_counts))
     ),
     Value = c(
       age_mean_sd, age_median_min_max_iqr,
       move_speed_mean_sd, move_speed_median_min_max_iqr,
+      weight_mean_sd, weight_median_min_max_iqr,
       as.vector(gender_counts),
-      as.vector(started_counts),
       as.vector(education_counts),
       as.vector(vr_experience_counts),
       as.vector(motion_counts),
-      as.vector(noticed_counts)
+      as.vector(condition_counts)
     )
   )
-  
+
   # Add the names of the counted categories to the details
   result$Category <- c(
-    "", "", "", "",
+    "", "", "", "", "", "",
     names(gender_counts),
-    names(started_counts),
     names(education_counts),
     names(vr_experience_counts),
     names(motion_counts),
-    names(noticed_counts)
+    names(condition_counts)
   )
-  
+
   return(result)
 }
 
@@ -136,10 +138,6 @@ started_with_noise <- function(participant) {
   return(get_p_results(participant, "noise_enabled", 2) == "True")
 }
 
-# did participant notice the VFD?
-noticed_vfd <- function(participant) {
-  return(get_p_detail(participant, "noticed") == "True")
-}
 
 # is it a practice trial
 is_practice <- function(participant, trial) {
@@ -158,9 +156,9 @@ get_t_data <- function(participant, trackerType, trialNum) {
     stop("Invalid tracker type specified.")
   }
 
-  filename <- paste0(filenameDict[[trackerType]],"_T", sprintf("%03d", trialNum), ".csv")
+  filename <- paste0(filenameDict[[trackerType]], "_T", sprintf("%03d", trialNum), ".csv")
   filePath <- file.path(get_p_dir(participant), "trackers", filename)
-  
+
   # Use tryCatch for more robust error handling
   tryCatch(
     {
